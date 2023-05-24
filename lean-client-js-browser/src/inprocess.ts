1333656e62231b67eb995e57d9c6e463eb89340d
Eric Wieser <wieser.eric@gmail.com>
2023-05-24 09:24:47 +0100

    Use new emscripten FS API
    `createFolder` no longer exists in new emscripten, the new name is `mkdir`

--- lean-client-js-browser/src/inprocess.ts
+++ lean-client-js-browser/src/inprocess.ts
@@ -109,7 +109,7 @@ export class InProcessTransport implements Transport {
             const BrowserFS = new FS();
             BrowserFS.initialize(libraryFS);
             const BFS = new EmscriptenFS(Module.FS, Module.PATH, Module.ERRNO_CODES, BrowserFS);
-            Module.FS.createFolder(Module.FS.root, 'library', true, true);
+            (Module.FS.mkdir || Module.FS.createFolder)(Module.FS.root, 'library', true, true);
             Module.FS.mount(BFS, {root: '/'}, '/library');
             this.info = library.urls;
         }
